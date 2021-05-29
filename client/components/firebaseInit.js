2d2f74a866ad5aca470156fff2bc94202aacf60e
shubham <shubhamranga@gmail.com>
2021-05-29 18:45:47 +0530

    firebase added

---
+++ client/components/firebaseInit.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBckAxCHmvWKnwfAbgfzGgDpAdFkE39pE4",
+  authDomain: "emo-track-d908e.firebaseapp.com",
+  projectId: "emo-track-d908e",
+  storageBucket: "emo-track-d908e.appspot.com",
+  messagingSenderId: "346401263246",
+  appId: "1:346401263246:web:61402e42998db2f7c4c3df",
+  measurementId: "G-155FH4Z942",
+};
+export default !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
