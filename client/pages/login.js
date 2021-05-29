afa1aedf8326216c972fd6e7b6413f4565a9a8e1
shubham <shubhamranga@gmail.com>
2021-05-29 19:34:29 +0530

    sign in with Google

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,51 +1,52 @@
 import Head from "next/head";
 import Image from "next/image";
-// import styles from "../styles/Home.module.css";
 import styled from "styled-components";
 import styles from "../styles/TIGOLBITTIES.module.scss";
 
-const FormContainer = styled.section`
-  background: black;
-  color: white;
-  width: 30em;
-  margin: 5em auto 10em auto;
-  padding: 3em;
-  border-radius: 20px;
-`;
+import firebaseApp from "../components/firebaseInit";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionDate } from "react-firebase-hooks/firestore";
 
-const FormHeading = styled.section`
-  color: white;
-  font-size: 2em;
-  text-align: center;
-  padding: 1em 0em 1em 0em;
-  border-radius: 20px;
-`;
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 export default function Login() {
+  const user = useAuthState(auth);
   return (
     <div>
-      <FormContainer>
-        <FormHeading>Login</FormHeading>
-        <div>
+      <section>
+        <h1>Login</h1>
+        {/* <div>
           <form>
             <input
-              class={styles.inputform}
+              className={styles.inputform}
               type="text"
               name="Username"
               placeholder="Username"
             ></input>
             <br></br>
-            {/* test */}
             <input
-              class={styles.inputform}
+              className={styles.inputform}
               type="text"
               name="Password"
               placeholder="Password"
             ></input>
             <button type="submit">Submit</button>
           </form>
-        </div>
-      </FormContainer>
+        </div> */}
+        <Signin></Signin>
+      </section>
     </div>
   );
+}
+
+function Signin() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
 }
